231445d65ac275b6bdef48cdd0700207430d323d
Baek, Chanwoo <qpp9946@naver.com>
2024-09-25 09:52:48 +0900

    fourth

--- src/e201814049/Main.java
+++ src/e201814049/Main.java
@@ -3,6 +3,7 @@ package e201814049;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("백찬우");
+		World.run();
 	}
 
 }
--- src/e201814049/World.java
+++ src/e201814049/World.java
@@ -1,5 +1,8 @@
 package e201814049;
 
 public class World {
+	public static void run() {
+		System.out.println("201814049");
+	}
 
 }
